92877b58c0e8f4e074f72713589c2c81f0053d12
SphericPlanet <gauranshpandey09@gmail.com>
2024-09-06 19:50:24 +0530

    Created Pyhthon First A.C.P

---
+++ main.py
@@ -0,0 +1,20 @@
+a=34
+b=4.5
+c="Gauransh Pandey"
+d=("ReplIt","VS Code","GitHub")
+x=True
+f = None   
+g = [1, 2, 3, 4] 
+h = {"name": "Gauransh", "age": 14} 
+k = range(5)    
+z=False
+print(a)
+print(b)
+print(c)
+print(d)
+print(x)
+print(f)
+print(g)
+print(h)
+print(k)
+print(z)
